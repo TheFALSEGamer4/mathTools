e9b2c1b91fe382293f57e95158ef9ee6c8fadabb
TheFALSEGamer4 <37382895+TheFALSEGamer4@users.noreply.github.com>
2020-04-20 13:10:11 -0500

    Update completegradecalculator.js

--- completegradecalculator.js
+++ completegradecalculator.js
@@ -9,30 +9,30 @@ function myFunction() {
 	var classGradeWithWeight = classGrade * 0.8;
 	var classGradeWithExam = examGradeWithWeight + classGradeWithWeight;
 	if (classGradeWithExam >= 98) {
-		document.getElementById("text").innerHTML = "Your letter grade is an A+ and your percentage is " + classGradeWithExam + "%.";
+		document.getElementById("text").innerHTML = "Your letter grade is an A+ and your percentage is " + classGradeWithExam.toFixed(2) + "%.";
 	} else if (classGradeWithExam < 98 && classGradeWithExam >= 93) {
-		document.getElementById("text").innerHTML = "Your letter grade is an A and your percentage is " + classGradeWithExam + "%.";
+		document.getElementById("text").innerHTML = "Your letter grade is an A and your percentage is " + classGradeWithExam.toFixed(2) + "%.";
 	} else if (classGradeWithExam < 93 && classGradeWithExam >= 90) {
-		document.getElementById("text").innerHTML = "Your letter grade is an A- and your percentage is " + classGradeWithExam + "%.";
+		document.getElementById("text").innerHTML = "Your letter grade is an A- and your percentage is " + classGradeWithExam.toFixed(2) + "%.";
 	} else if (classGradeWithExam < 90 && classGradeWithExam >= 87) {
-		document.getElementById("text").innerHTML = "Your letter grade is a B+ and your percentage is " + classGradeWithExam + "%.";
+		document.getElementById("text").innerHTML = "Your letter grade is a B+ and your percentage is " + classGradeWithExam.toFixed(2) + "%.";
 	} else if (classGradeWithExam < 87 && classGradeWithExam >= 83) {
-		document.getElementById("text").innerHTML = "Your letter grade is a B and your percentage is " + classGradeWithExam + "%.";
+		document.getElementById("text").innerHTML = "Your letter grade is a B and your percentage is " + classGradeWithExam.toFixed(2) + "%.";
 	} else if (classGradeWithExam < 83 && classGradeWithExam >= 80) {
-		document.getElementById("text").innerHTML = "Your letter grade is a B- and your percentage is " + classGradeWithExam + "%.";
+		document.getElementById("text").innerHTML = "Your letter grade is a B- and your percentage is " + classGradeWithExam.toFixed(2) + "%.";
 	} else if (classGradeWithExam < 80 && classGradeWithExam >= 77) {
-		document.getElementById("text").innerHTML = "Your letter grade is a C+ and your percentage is " + classGradeWithExam + "%.";
+		document.getElementById("text").innerHTML = "Your letter grade is a C+ and your percentage is " + classGradeWithExam.toFixed(2) + "%.";
 	} else if (classGradeWithExam < 77 && classGradeWithExam >= 73) {
-		document.getElementById("text").innerHTML = "Your letter grade is a C and your percentage is " + classGradeWithExam + "%.";
+		document.getElementById("text").innerHTML = "Your letter grade is a C and your percentage is " + classGradeWithExam.toFixed(2) + "%.";
 	} else if (classGradeWithExam < 73 && classGradeWithExam >= 70) {
-		document.getElementById("text").innerHTML = "Your letter grade is a C- and your percentage is " + classGradeWithExam + "%.";
+		document.getElementById("text").innerHTML = "Your letter grade is a C- and your percentage is " + classGradeWithExam.toFixed(2) + "%.";
 	} else if (classGradeWithExam < 70 && classGradeWithExam >= 67) {
-		document.getElementById("text").innerHTML = "Your letter grade is a D+ and your percentage is " + classGradeWithExam + "%.";
+		document.getElementById("text").innerHTML = "Your letter grade is a D+ and your percentage is " + classGradeWithExam.toFixed(2) + "%.";
 	} else if (classGradeWithExam < 67 && classGradeWithExam >= 63) {
-		document.getElementById("text").innerHTML = "Your letter grade is a D and your percentage is " + classGradeWithExam + "%.";
+		document.getElementById("text").innerHTML = "Your letter grade is a D and your percentage is " + classGradeWithExam.toFixed(2) + "%.";
 	} else if (classGradeWithExam < 63 && classGradeWithExam >= 60) {
-		document.getElementById("text").innerHTML = "Your letter grade is a D- and your percentage is " + classGradeWithExam + "%.";
+		document.getElementById("text").innerHTML = "Your letter grade is a D- and your percentage is " + classGradeWithExam.toFixed(2) + "%.";
 	} else if (classGradeWithExam < 60) {
-		document.getElementById("text").innerHTML = "Your letter grade is a F and your percentage is " + classGradeWithExam + "%.";
+		document.getElementById("text").innerHTML = "Your letter grade is a F and your percentage is " + classGradeWithExam.toFixed(2) + "%.";
 	}
 }
