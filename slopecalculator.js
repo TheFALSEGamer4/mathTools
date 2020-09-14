4676f0f2440587fd9d78e36c9b75b77b8e27b224
FALSEGamer4 <37382895+TheFALSEGamer4@users.noreply.github.com>
2020-09-13 19:26:05 -0500

    Removed the focus from js and put into html

--- slopecalculator.js
+++ slopecalculator.js
@@ -1,35 +1,3 @@
-window.onload = function () {
-	document.getElementById("x1").focus();
-	var input1 = document.getElementById("x1");
-	input1.addEventListener("keyup", function (event) {
-		if (event.keyCode === 13) {
-			event.preventDefault();
-			document.getElementById("y1").focus();
-		}
-	});
-	var input2 = document.getElementById("y1");
-	input2.addEventListener("keyup", function (event) {
-		if (event.keyCode === 13) {
-			event.preventDefault();
-			document.getElementById("x2").focus();
-		}
-	});
-	var input3 = document.getElementById("x2");
-	input3.addEventListener("keyup", function (event) {
-		if (event.keyCode === 13) {
-			event.preventDefault();
-			document.getElementById("y2").focus();
-		}
-	});
-	var input4 = document.getElementById("y2");
-	input4.addEventListener("keyup", function (event) {
-		if (event.keyCode === 13) {
-			event.preventDefault();
-			formula();
-		}
-	});
-}
-
 function formula() {
 	var x1 = Number(document.getElementById("x1").value);
 	var x2 = Number(document.getElementById("x2").value);
